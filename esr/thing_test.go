7a4b27cc71d8a152e8dc5bd7c0900ed72f4ae45f
walpat-1 <walpat-1@student.ltu.se>
2025-07-14 18:10:03 +0200

    Added help functions etc for testing FilterByServiceDefinitionAndDetails()

--- esr/thing_test.go
+++ esr/thing_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/sdoque/mbaigo/forms"
@@ -42,11 +43,101 @@ func TestUnmarshalTraits(t *testing.T) {}
 // (ua *UnitAsset) serviceRegistryHandler() {}
 func TestServiceRegistryHandler(t *testing.T) {}
 
-// (ua *UnitAsset) FilterByServiceDefinitionAndDetails(desiredDefinition string, requiredDetails map[string][]string) []forms.ServiceRecord_v1 {
-func TestFilterByServiceDefAndDetails(t *testing.T) {}
+// ------------------------------------------------------------------------ //
+// Help functions and structs to test FilterByServiceDefinitionAndDetails()
+// ------------------------------------------------------------------------ //
 
-// checkExpiration(ua *UnitAsset, servId int) {}
-func TestCheckExpiration(t *testing.T) {}
+func createAssetWithServices() (ua *UnitAsset, err error) {
+	initTemp := initTemplate()
+	ua, ok := initTemp.(*UnitAsset)
+	if !ok {
+		return nil, fmt.Errorf("Failed while typecasting to local UnitAsset")
+	}
+	locations := []string{"Kitchen", "Bathroom"}
+	ua.serviceRegistry = make(map[int]forms.ServiceRecord_v1)
+	for i, l := range locations {
+		var form forms.ServiceRecord_v1
+		form.ServiceDefinition = "testDef"
+		form.SystemName = fmt.Sprintf("testSystem%d", i)
+		form.ProtoPort = map[string]int{"http": i}
+		form.IPAddresses = []string{fmt.Sprintf("999.999.%d.999", i)}
+		form.EndOfValidity = "2026-01-02T15:04:05Z"
+		form.Details = make(map[string][]string)
+		form.Details = map[string][]string{"Location": {l}}
+		ua.serviceRegistry[i] = form
+	}
+	return ua, nil
+}
+
+func TestFilterByServiceDefAndDetails(t *testing.T) {
+	ua, err := createAssetWithServices()
+	if err != nil {
+		t.Errorf("failed while creating asset at startup")
+	}
+	a := map[string][]string{"Location": {"Bathroom"}}
+	lst := ua.FilterByServiceDefinitionAndDetails("testDef", a)
+	log.Printf("LST: %+v", lst)
+}
+
+// ---------------------------------------------------- //
+// Help functions and structs to test checkExpiration()
+// ---------------------------------------------------- //
+
+func createAssetWithService(year any) (ua *UnitAsset, err error) {
+	initTemp := initTemplate()
+	ua, ok := initTemp.(*UnitAsset)
+	if !ok {
+		return nil, fmt.Errorf("Failed while typecasting to local UnitAsset")
+	}
+
+	var test forms.ServiceRecord_v1
+	test.SystemName = "testSystem"
+	test.ProtoPort = map[string]int{"http": 1234}
+	test.IPAddresses = []string{"999.999.999.999"}
+	test.EndOfValidity = fmt.Sprintf("%v-01-02T15:04:05Z", year)
+	ua.serviceRegistry = map[int]forms.ServiceRecord_v1{0: test}
+	return ua, nil
+}
+
+type checkExpirationParams struct {
+	servicePresent bool
+	setup          func() (*UnitAsset, error)
+	testCase       string
+}
+
+func TestCheckExpiration(t *testing.T) {
+	params := []checkExpirationParams{
+		{
+			true,
+			func() (ua *UnitAsset, err error) { return createAssetWithService(2026) },
+			"Best case, service not past expiration",
+		},
+		{
+			false,
+			func() (ua *UnitAsset, err error) { return createAssetWithService(2006) },
+			"Bad case, service past expiration",
+		},
+		{
+			true,
+			func() (ua *UnitAsset, err error) { return createAssetWithService("faulty") },
+			"Bad case, time parsing problem",
+		},
+	}
+	for _, c := range params {
+		ua, err := c.setup()
+		if err != nil {
+			t.Errorf("failed during setup: %v", err)
+		}
+
+		checkExpiration(ua, 0)
+		if _, exists := ua.serviceRegistry[0]; (exists == false) && (c.servicePresent == true) {
+			t.Errorf("expected the service to be present in '%s'", c.testCase)
+		}
+		if _, exists := ua.serviceRegistry[0]; (exists == true) && (c.servicePresent == false) {
+			t.Errorf("expected service to be removed in '%s'", c.testCase)
+		}
+	}
+}
 
 // ----------------------------------------------------- //
 // Help functions and structs to test getUniqueSystems()

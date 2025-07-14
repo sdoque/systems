66319c0efc8a679f845531b7edbb21fcf5619586
walpat-1 <walpat-1@student.ltu.se>
2025-07-14 15:35:41 +0200

    Added test for getUniqueSystems()

--- esr/thing_test.go
+++ esr/thing_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sdoque/mbaigo/forms"
+)
 
 // ----------------------------------------------------- //
 // Help functions and structs to tests initTemplate()
@@ -41,9 +46,96 @@ func TestServiceRegistryHandler(t *testing.T) {}
 func TestFilterByServiceDefAndDetails(t *testing.T) {}
 
 // checkExpiration(ua *UnitAsset, servId int) {}
-func TestCheckExpiration(t *testing.T) {
-	t.Errorf("Next test in line")
+func TestCheckExpiration(t *testing.T) {}
+
+// ----------------------------------------------------- //
+// Help functions and structs to test getUniqueSystems()
+// ----------------------------------------------------- //
+
+func createServRegistryHttp() (ua *UnitAsset, err error) {
+	initTemp := initTemplate()
+	ua, ok := initTemp.(*UnitAsset)
+	if !ok {
+		return nil, fmt.Errorf("Failed while typecasting to local UnitAsset")
+	}
+
+	var test forms.ServiceRecord_v1
+	test.SystemName = "testSystem"
+	test.ProtoPort = map[string]int{"http": 1234}
+	test.IPAddresses = []string{"999.999.999.999"}
+	ua.serviceRegistry = map[int]forms.ServiceRecord_v1{0: test}
+
+	return ua, nil
 }
 
-// getUniqueSystems(ua *UnitAsset) (*forms.SystemRecordList_v1, error) {}
-func TestGetUniqueSystems(t *testing.T) {}
+func createServRegistryHttps() (ua *UnitAsset, err error) {
+	initTemp := initTemplate()
+	ua, ok := initTemp.(*UnitAsset)
+	if !ok {
+		return nil, fmt.Errorf("Failed while typecasting to local UnitAsset")
+	}
+
+	var test forms.ServiceRecord_v1
+	test.SystemName = "testSystem"
+	test.ProtoPort = map[string]int{"https": 4321}
+	test.IPAddresses = []string{"888.888.888.888"}
+	ua.serviceRegistry = map[int]forms.ServiceRecord_v1{0: test}
+
+	return ua, nil
+}
+
+func createBrokenServRegistry() (ua *UnitAsset, err error) {
+	initTemp := initTemplate()
+	ua, ok := initTemp.(*UnitAsset)
+	if !ok {
+		return nil, fmt.Errorf("Failed while typecasting to local UnitAsset")
+	}
+
+	var test forms.ServiceRecord_v1
+	test.SystemName = "testSystem"
+	test.ProtoPort = map[string]int{"https": 0}
+	test.IPAddresses = []string{"888.888.888.888"}
+	ua.serviceRegistry = map[int]forms.ServiceRecord_v1{0: test}
+	return ua, nil
+}
+
+type getUniqueSystemsParams struct {
+	expectError bool
+	setup       func() (ua *UnitAsset, err error)
+	testCase    string
+}
+
+func TestGetUniqueSystems(t *testing.T) {
+	params := []getUniqueSystemsParams{
+		{
+			false,
+			func() (ua *UnitAsset, err error) { return createServRegistryHttp() },
+			"Best case, http",
+		},
+		{
+			false,
+			func() (ua *UnitAsset, err error) { return createServRegistryHttps() },
+			"Best case, https",
+		},
+		{
+			false,
+			func() (ua *UnitAsset, err error) { return createBrokenServRegistry() },
+			"Bad case, http/https not found",
+		},
+	}
+
+	for _, c := range params {
+		ua, err := c.setup()
+		if err != nil {
+			t.Errorf("Failed during setup in '%s' with error: %v", c.testCase, err)
+		}
+		_, err = getUniqueSystems(ua)
+		//log.Printf("sys: %+v", sys)
+		if c.expectError == false && err != nil {
+			t.Errorf("Failed while getting unique systems in '%s': %v", c.testCase, err)
+		}
+		if c.expectError == true && err == nil {
+			t.Errorf("Expected errors in '%s'", c.testCase)
+		}
+	}
+}
